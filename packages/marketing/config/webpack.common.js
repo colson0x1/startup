c2b07a42ef07f51c10070199864ff03544f40770
Colson <colsoncalifornication@gmail.com>
2024-06-03 11:37:06 +0545

    marketing project: configure common, dev and prod version of webpack for running in different modes: merge development config and with common webpack config

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      /* @ Loader */
+
+      /* @ Babel */
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+};
