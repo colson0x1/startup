8fe35ac825bc7719c3843d02e8039a6dbf3d5c5c
Colson <colsoncalifornication@gmail.com>
2024-06-04 16:06:42 +0545

    configure Container Application Webpack for production: add environment variable for remote marketing production domain that is going to be defined when building the application in production through CI/CD pipeline that is production remote domain will be knwon when all Applications are about to be deployed and all infrastructure are created on AWS + refactor: move html webpack plugin from webpack dev config to common config so it can be used for both development and production build of webpack + add a build script to Container package.json to make sure everything can be build for production

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   module: {
     rules: [
@@ -22,4 +24,9 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
