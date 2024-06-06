2c5d6ee9b1506934b9478e265e07160fb13effe1
Colson <colsoncalifornication@gmail.com>
2024-06-07 00:01:36 +0545

    resolve minor bug related to loading index html on historyApiFallback in webpack development configuration for Container and Marketing Microfrontends - hotfix 404 errors in certain situations such as directly accessing http://localhost:8081/pricing

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -8,8 +8,9 @@ const devConfig = {
   mode: 'development',
   devServer: {
     port: 8081,
+    // historyApiFallback: true,
     historyApiFallback: {
-      index: 'index.html',
+      index: '/index.html',
     },
   },
   plugins: [
