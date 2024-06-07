11cb4f5d93a2dc1526ebd84baaa130ca69750695
Colson <colsoncalifornication@gmail.com>
2024-06-07 17:21:05 +0545

    use Browser History in isolation: make sure Marketing can be ran in isolation and see the currently visited actual url - choose browser history object instead of memory history object when running Marketing in development

--- packages/marketing/src/bootstrap.jsx
+++ packages/marketing/src/bootstrap.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the ap
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot, {});
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
