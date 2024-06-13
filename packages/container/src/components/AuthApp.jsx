28398566b528f82d8a7ab35f6eaca7bb23d53f43
Colson <colsoncalifornication@gmail.com>
2024-06-13 17:35:12 +0545

    communicate authentication state: create isSignedIn piece of state + use isSignedIn to update the header login button

--- packages/container/src/components/AuthApp.jsx
+++ packages/container/src/components/AuthApp.jsx
@@ -2,7 +2,7 @@ import { mount } from 'auth/AuthApp';
 import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default () => {
+export default ({ onSignIn }) => {
   const ref = useRef(null);
   const history = useHistory();
 
@@ -20,9 +20,11 @@ export default () => {
           history.push(nextPathname);
         }
       },
-      onSignIn: () => {
-        console.log('User signed in');
-      },
+      /* onSignIn: () => {
+        // console.log('User signed in');
+        onSignIn();
+      }, */
+      onSignIn,
     });
 
     history.listen(onParentNavigate);
--- packages/container/src/components/Header.jsx
+++ packages/container/src/components/Header.jsx
@@ -54,11 +54,11 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Header({ signedIn, onSignOut }) {
+export default function Header({ isSignedIn, onSignOut }) {
   const classes = useStyles();
 
   const onClick = () => {
-    if (signedIn && onSignOut) {
+    if (isSignedIn && onSignOut) {
       onSignOut();
     }
   };
@@ -66,30 +66,30 @@ export default function Header({ signedIn, onSignOut }) {
   return (
     <React.Fragment>
       <AppBar
-        position="static"
-        color="default"
+        position='static'
+        color='default'
         elevation={0}
         className={classes.appBar}
       >
         <Toolbar className={classes.toolbar}>
           <Typography
-            variant="h6"
-            color="inherit"
+            variant='h6'
+            color='inherit'
             noWrap
             component={RouterLink}
-            to="/"
+            to='/'
           >
             App
           </Typography>
           <Button
-            color="primary"
-            variant="outlined"
+            color='primary'
+            variant='outlined'
             className={classes.link}
             component={RouterLink}
-            to={signedIn ? '/' : '/auth/signin'}
+            to={isSignedIn ? '/' : '/auth/signin'}
             onClick={onClick}
           >
-            {signedIn ? 'Logout' : 'Login'}
+            {isSignedIn ? 'Logout' : 'Login'}
           </Button>
         </Toolbar>
       </AppBar>
