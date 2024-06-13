2c76c2e662de7a1258740293d1cd05a220149472
Colson <colsoncalifornication@gmail.com>
2024-06-13 23:04:04 +0545

    run Dashboard Microfrontend in isolation: add dashboard component in vue: render the dashboard + refactor bootstrap: mount vue app to the element in the DOM

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,22 @@
+/* @ Dashboard Microfrontend */
+
+import { createApp } from 'vue';
+import Dashboard from './components/Dashboard.vue';
+
+// Mount function to start up the ap
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
+};
+
+// If we are in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_dashboard-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// We are running through container and we should export the mount function
+export { mount };
