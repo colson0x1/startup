3909895867248af03a4da826e8810204258c6212
Colson <colsoncalifornication@gmail.com>
2024-06-13 22:47:06 +0545

    initial Dashboard Microfrontend setup: add dashboard sub application - configure webpack common js file for vue based app + configure scripts on package json for webpack

---
+++ packages/dashboard/config/webpack.common.js
@@ -0,0 +1,46 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+  module: {
+    rules: [
+      /* @ Loader */
+
+      /* @ file loader */
+      {
+        test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
+        use: [{ loader: 'file-loader' }],
+      },
+      /* @ vue loader */
+      {
+        test: /\.vue$/,
+        use: 'vue-loader',
+      },
+      {
+        test: /\.scss|\.css$/,
+        use: ['vue-style-loader', 'style-loader', 'css-loader', 'sass-loader'],
+      },
+      /* @ Babel */
+      {
+        // test: /\.m?js$/,
+        // Enable support for JSX
+        test: /\.(m?js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
---
+++ packages/dashboard/src/bootstrap.jsx
@@ -0,0 +1,46 @@
+/* @ Dashboard Microfrontend */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+// Mount function to start up the ap
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      // console.log('Container just navigated');
+      // console.log(location);
+
+      const { pathname } = history.location;
+      // console.log(nextPathname);
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// If we are in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// We are running through container and we should export the mount function
+export { mount };
